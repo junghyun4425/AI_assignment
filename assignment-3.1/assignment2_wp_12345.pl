19b85302d1da2e2ac6a7c116d713ffba4e547e02
Your Name <lo18144@bristol.ac.uk>
2019-04-21 20:31:01 +0100

    BITTOR Part 2 works, Part 1 needs finishing to avoid running out of energy

--- assignment-3.1/assignment2_wp_12345.pl
+++ assignment-3.1/assignment2_wp_12345.pl
@@ -7,13 +7,15 @@ find_identity(A):-
   ; otherwise -> find_identity_o(A)
   ).
 
-possible(Links,A) :- actor(A), wp:actor_links(A,L), forall(member(Link,Links),member(Link,L)).
+possible(Links,A,Actors) :- member(A,Actors), wp:actor_links(A,L),
+                            forall(member(Link,Links),member(Link,L)).
 
 iterate([A],A,_).
-iterate(_,A,Links) :-
+iterate(Actors,A,Links) :-
       agent_ask_oracle(oscar,o(1),link,L),
-      (\+member(L,Links) -> append([L],Links,NewLinks) ; otherwise -> NewLinks = Links),
-      findall(Actor,possible(NewLinks,Actor),NewActors), iterate(NewActors,A,NewLinks).
+      (not(member(L,Links)) -> NewLinks = [L|Links], findall(A,possible(NewLinks,A,Actors),NewActors)
+      ; otherwise -> NewLinks = Links, NewActors = Actors),
+      iterate(NewActors,A,NewLinks).
 
 find_identity_2(A):-
   findall(Actor,actor(Actor),Actors), iterate(Actors,A,[]),!.
