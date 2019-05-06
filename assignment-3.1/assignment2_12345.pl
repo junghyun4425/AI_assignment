0038e8da102ee9ccd4a791397f6b33626b99b62e
Your Name <lo18144@bristol.ac.uk>
2019-05-06 18:18:54 +0100

    Part3 done

--- assignment-3.1/assignment2_12345.pl
+++ assignment-3.1/assignment2_12345.pl
@@ -1,20 +1,41 @@
 candidate_number(12345).
 
+get_energy(E) :- my_agent(Agent),query_world( agent_current_energy, [Agent,E] ).
+
+recharge :-   my_agent(Agent),
+              query_world( agent_current_position, [Agent,P] ),
+              solve_task_astar(find(c(I)),[[0,_,P,[P]]],R,_Cost,_NewPos,[]),!,
+              reverse(R,[_Init|Path]),
+              query_world( agent_do_moves, [Agent,Path] ),
+              query_world( agent_topup_energy, [Agent,c(I)] ).
+
 solve_task(Task,Cost):-
   my_agent(Agent),
   query_world( agent_current_position, [Agent,P] ),
-  solve_task_astar(Task,[[0,_,P,[P]]],R,Cost,_NewPos),!,  % prune choice point for efficiency
-  reverse(R,[_Init|Path]),
-  query_world( agent_do_moves, [Agent,Path] ).
+  solve_task_astar(Task,[[0,_,P,[P]]],R,Cost,_NewPos,[]),!,  % prune choice point for efficiency
+  get_energy(Energy), Cost = [cost(C)|_],
+  (Energy - C < 35 ->  recharge,
+                          solve_task(Task,_Cost)
+  ;
+  otherwise ->  reverse(R,[_Init|Path]),
+                query_world( agent_do_moves, [Agent,Path] )
+  ).
 
-
-% Find whether a position P is in the agenda
-in_list(P,[[_,_,P,_]|_]).
-in_list(P,[_|T]) :- in_list(P,T).
+solve_task(Task,Cost,Banned):-
+  my_agent(Agent),
+  query_world( agent_current_position, [Agent,P] ),
+  solve_task_astar(Task,[[0,_,P,[P]]],R,Cost,_NewPos,Banned),!,  % prune choice point for efficiency
+  get_energy(Energy), Cost = [cost(C)|_],
+  (Energy - C < 35 ->  recharge,
+                          solve_task(Task,_Cost)
+  ;
+  otherwise ->  reverse(R,[_Init|Path]),
+                query_world( agent_do_moves, [Agent,Path] )
+  ).
 
 children([G,_,P,RPath],[G1,H1,P1,[P1|RPath]],Task,Agenda) :- search(P,P1,_,_), G1 is G + 1,
                                                       heuristics(P1,H1,Task),
-                                                      not(in_list(P1,Agenda)).
+                                                      not(member([_,_,P1,_],Agenda)).
 
 target(T,go(Exit)) :- T = Exit.
 target(T,find(O)) :- query_world( check_pos, [T, O]). % It does not work, cannot find the T
@@ -27,24 +48,24 @@ find_min_aux(Smallest,Seen,[],[Smallest|Seen]).
 find_min_aux(Smallest,Seen,[H|T],Result) :- lt(H,Smallest),!,find_min_aux(H,[Smallest|Seen],T,Result) ; find_min_aux(Smallest,[H|Seen],T,Result).
 lt([G1,H1,_,_],[G2,H2,_,_]) :- F1 is G1 + H1, F2 is G2 + H2, F1 < F2.
 
-solve_task_astar(Task,Agenda,RPath,[cost(Cost),depth(Cost)],NewPos) :-
+solve_task_astar(Task,Agenda,RPath,[cost(Cost),depth(Cost)],NewPos,Banned) :-
   find_min(Agenda,TopFirst),
-  achieved(Task,TopFirst,RPath,Cost,NewPos).
-solve_task_astar(Task,Agenda,RR,Cost,NewPos) :-
+  achieved(Task,TopFirst,RPath,Cost,NewPos,Banned).
+solve_task_astar(Task,Agenda,RR,Cost,NewPos,Banned) :-
   find_min(Agenda,[M|T]),
   (setof(Child,children(M,Child,Task,T),Children) -> append(Children,T,NewAgenda) ;
   otherwise -> NewAgenda = T),
-  solve_task_astar(Task,NewAgenda,RR,Cost,NewPos).
+  solve_task_astar(Task,NewAgenda,RR,Cost,NewPos,Banned).
 
-achieved(go(Exit),Agenda,RPath,Cost,NewPos) :-
+achieved(go(Exit),Agenda,RPath,Cost,NewPos,_Banned) :-
   Agenda = [[Cost,_,NewPos,RPath]|_],
   ( Exit=none -> true
   ; otherwise -> RPath = [Exit|_]
   ).
-achieved(find(O),Agenda,RPath,Cost,NewPos) :-
+achieved(find(O),Agenda,RPath,Cost,NewPos,Banned) :-
   Agenda = [[Cost,_,NewPos,RPath]|_],
   ( O=none    -> true
-  ; otherwise -> RPath = [Last|_],map_adjacent(Last,_,O)
+  ; otherwise -> RPath = [Last|_],map_adjacent(Last,_,O),not(member(O,Banned))
   ).
 
 search(F,N,N,1) :-
--- assignment-3.1/assignment2_wp_12345.pl
+++ assignment-3.1/assignment2_wp_12345.pl
@@ -7,18 +7,28 @@ find_identity(A):-
   ; otherwise -> find_identity_o(A)
   ).
 
-possible(Links,A,Actors) :- member(A,Actors), wp:actor_links(A,L),
-                            forall(member(Link,Links),member(Link,L)).
+possible(Links,A) :- actor(A), wp:actor_links(A,L), forall(member(Link,Links),member(Link,L)).
 
 iterate([A],A,_).
-iterate(Actors,A,Links) :-
+iterate(_,A,Links) :-
       agent_ask_oracle(oscar,o(1),link,L),
-      (not(member(L,Links)) -> NewLinks = [L|Links], findall(A,possible(NewLinks,A,Actors),NewActors)
-      ; otherwise -> NewLinks = Links, NewActors = Actors),
-      iterate(NewActors,A,NewLinks).
+      (\+member(L,Links) -> NewLinks = [L|Links] ; otherwise -> NewLinks = Links),
+      findall(Actor,possible(NewLinks,Actor),NewActors), iterate(NewActors,A,NewLinks).
 
 find_identity_2(A):-
   findall(Actor,actor(Actor),Actors), iterate(Actors,A,[]),!.
 
+iterate_o([A],A,_,_,_).
+iterate_o(Actors,A,Links,N,Visited) :-
+                          my_agent(Agent),
+                          query_world( agent_current_position, [Agent,P] ),
+                          N1 is N + 1,
+                          solve_task(find(o(I)),_,Visited),
+                          query_world(agent_ask_oracle, [Agent,o(I),link,L]),
+                          (\+member(L,Links) -> append([L],Links,NewLinks) ; otherwise -> NewLinks = Links),
+                          findall(Actor,possible(NewLinks,Actor),NewActors),
+                          iterate_o(NewActors,A,NewLinks,N1,[o(I)|Visited]).
+
 find_identity_o(A):-
-  A='Not yet implemented'.
+  findall(Actor,actor(Actor),Actors),
+  iterate_o(Actors,A,[],0,[]).
